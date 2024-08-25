34af5d7d2f22e7702cb1482a5f4c52de4737258a
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-25 19:08:46 +0530

    Delete data_preprocessing.py

--- data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-# data_preprocessing.py
-
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-def preprocess_data(df):
-    scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df)
-    return pd.DataFrame(df_scaled, columns=df.columns)
